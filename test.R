320e57f841bf3986919e43792b3836bf71aed32f
Oier Zugazaga <oier.z@opendeusto.es>
2025-11-24 09:42:18 +0100

    yets

--- test.R
+++ test.R
@@ -1,4 +1,5 @@
 library(tibble)
+library(ggplot2)
 
 #CARGAR SOLO 1 VEZ
 csv <- read.csv("kddcup.csv")
@@ -17,3 +18,22 @@ print(netWorkDataset)
 
 #OMMIT NA's
 netWorkDataset <- na.omit(netWorkDataset)
+
+# Convertir columnas character a factor
+char_cols <- sapply(netWorkDataset, is.character)
+netWorkDataset[, char_cols] <- lapply(netWorkDataset[, char_cols], as.factor)
+
+# Convertir columnas numéricas con menos de 30 niveles a factor
+num_cols <- sapply(netWorkDataset, is.numeric)
+for (colname in names(netWorkDataset)[num_cols]) {
+  if (length(unique(netWorkDataset[[colname]])) < 20) {
+    netWorkDataset[[colname]] <- as.factor(netWorkDataset[[colname]])
+  }
+}
+
+#str(dataset) → Ver tipos de datos y estructura
+#summary(dataset) → Estadísticas básicas, detecta NAs, outliers
+#head(dataset) / tail(dataset) → Visualizar primeras y últimas filas
+#dim(dataset) → Tamaño (filas y columnas)
+#names(dataset) → Nombres de columnas
+
